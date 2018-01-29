cb7854404bc0b9fe0bbc472d75050412652cc9cc
AlmasB <almaslvl@gmail.com>
2018-01-29 23:00:34 +0000

    added missing test branch

--- src/test/kotlin/com/almasb/tracery/TraceryTest.kt
+++ src/test/kotlin/com/almasb/tracery/TraceryTest.kt
@@ -86,7 +86,7 @@ class TraceryTest {
         val grammar = Tracery.createGrammar(json)
         val expandedText = grammar.flatten("sentence")
 
-        assertThat(expandedText, `is`("Purple unicorns are always indignant. A kitten is impassioned, unless it is an orange one."))
+        assertThat(expandedText, `is`("Purple unicorns are always indignant. A kitten is impassioned, unless it is an orange one. Truly!"))
     }
 
     @ParameterizedTest
